c71f1c2d0916ab4a87aa47219653bf9253302f95
Somesh Jha <someshj@ca.ibm.com>
2017-07-26 12:47:01 -0300

    Add GET /todos app

--- server/server.js
+++ server/server.js
@@ -23,6 +23,17 @@ app.post('/todo', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({
+            todos
+        })
+    }, (e) =>{
+        res.status(400).send(e);
+    })
+});
+
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
