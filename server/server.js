50012b3db8485afe00b9b3f7009513b42b5b892b
Somesh Jha <someshj@ca.ibm.com>
2017-07-27 00:13:55 -0300

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const {ObjectId} = require('mongodb');
 
 
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -54,8 +56,8 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send(e);
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
