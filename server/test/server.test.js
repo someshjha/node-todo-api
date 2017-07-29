714a9eb124e8074fb82532bcbcf36186f21b6f46
Somesh Jha <someshj@ca.ibm.com>
2017-07-29 18:22:58 -0300

    Test for GET /users/me

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,28 +4,17 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-}, {
-     _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}];
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 
 describe('POST /todo', () =>{
     it('should create a new todo', (done) => {
         var text = 'Test todo text';
-
         request(app)
             .post('/todo')
             .send({text})
@@ -174,4 +163,28 @@ describe('PATCH /todos/:id', () => {
             })
             .end(done());
     });
+});
+
+ describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
 });
